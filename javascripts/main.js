359b4a5e5c931775365f4fe3a608c2477ada3109
yamazakiM <yamazakiM@users.noreply.github.com>
2016-03-28 15:00:27 +0900

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,22 @@
 console.log('This would be the main JS file.');
 
 $(function() {
+    var showFlag = false;
     var topBtn = $('#page-top');    
-    topBtn.hide();
+    topBtn.css('bottom', '-100px');
+    var showFlag = false;
     //スクロールが100に達したらボタン表示
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
-            topBtn.fadeIn();
+            if (showFlag == false) {
+                showFlag = true;
+                topBtn.stop().animate({'bottom' : '20px'}, 200); 
+            }
         } else {
-            topBtn.fadeOut();
+            if (showFlag) {
+                showFlag = false;
+                topBtn.stop().animate({'bottom' : '-100px'}, 200); 
+            }
         }
     });
     //スクロールしてトップ
