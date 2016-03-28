259a00c1718e8e52bd67b70549c585bcb89b1ece
yamazakiM <yamazakiM@users.noreply.github.com>
2016-03-28 14:47:56 +0900

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,21 @@
 console.log('This would be the main JS file.');
+
+$(function() {
+    var topBtn = $('#page-top');    
+    topBtn.hide();
+    //スクロールが100に達したらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップ
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
