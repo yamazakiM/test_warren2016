cc1823d6280baf0f00260942cc80a538b394bcac
yamazakiM <yamazakiM@users.noreply.github.com>
2016-03-30 13:20:52 +0900

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,14 @@
 console.log('This would be the main JS file.');
 
+// page Topフェードイン・アウト
+$(function(){
+    $(window).bind("scroll", function() {
+    // トップから150px以上スクロールしたら
+    if ($(this).scrollTop() > 150) { 
+     // ページトップのリンクをフェードインする
+        $(".pageTop").fadeIn();
+    } else { // それ以外は
+     // ページトップのリンクをフェードアウトする
+        $(".pageTop").fadeOut();
+    }
+});
