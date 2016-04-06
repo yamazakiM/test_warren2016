1b5760e01e60db01fdf3bfa41e81db496fcb37ba
yamazakiM <yamazakiM@users.noreply.github.com>
2016-04-06 15:52:09 +0900

    Create custom.js

---
+++ javascripts/custom.js
@@ -0,0 +1,12 @@
+function mainmenu(){
+$(" #globalNavi li").hover(function(){
+        $(this).find('ul:first').css({visibility: "visible",display: "none"}).show(600);//※表示速度変更は、show(600)の数字を変更で可能。
+        },function(){
+        $(this).find('ul:first').css({visibility: "hidden"});
+        });
+}
+ 
+  
+  
+$(document).ready(function(){                   
+    mainmenu();
